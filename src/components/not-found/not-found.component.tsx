1bbbe87e1a940b906c0da599cfd42af5c1a9e161
Farasat Ali <farasataliazeemi1@gmail.com>
2021-03-19 10:42:42 +0500

    Updated App

--- src/components/not-found/not-found.component.tsx
+++ src/components/not-found/not-found.component.tsx
@@ -4,16 +4,16 @@ import "./not-found.styles.css"
 
 const NotFoundComponent = () => {
   return (
-    <section className="bookmark-component__not-found">
-      <div className="bookmark-component__not-found__container">
-        <div className="bookmark-component__not-found__bg">
+    <section className="lolly-component__not-found">
+      <div className="lolly-component__not-found__container">
+        <div className="lolly-component__not-found__bg">
           <h1>404</h1>
         </div>
 
-        <div className="bookmark-component__not-found__instructions">
+        <div className="lolly-component__not-found__instructions">
           <h3>Look like you're lost 🤔</h3>
           <p>The page you are looking for not available!</p>
-          <Link to="/" className="bookmark-component__not-found__404">
+          <Link to="/" className="lolly-component__not-found__404">
             Go To Home
           </Link>
         </div>
