31247da96a9a1d0e36835be70ac1521d900f5886
Farasat Ali <farasataliazeemi1@gmail.com>
2021-03-19 10:39:14 +0500

    Updated the App

--- src/components/layout/layout.component.tsx
+++ src/components/layout/layout.component.tsx
@@ -1,14 +1,10 @@
 import * as React from "react"
 import "./layout.styles.css"
-import HeaderComponent from "../header/header.component"
-import FooterComponent from "../footer/footer.component"
 
 const Layout = ({ children }) => {
   return (
     <>
-      <HeaderComponent />
       <main className="bookmark-component__layout-main">{children}</main>
-      <FooterComponent />
     </>
   )
 }
