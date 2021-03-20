5d20e445c97610396aad569f3a818776f19e7827
Farasat Ali <farasataliazeemi1@gmail.com>
2021-03-20 11:23:49 +0500

    Created LollyTextUndefined Component

---
+++ src/components/lolly-text-undefined/lolly-text-undefined.component.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import "./lolly-text-undefined.styles.css"
+
+const LollyTextUndefinedComponent = () => {
+  return (
+    <div>
+      <h1>Lolly Text Undefined</h1>
+    </div>
+  )
+}
+
+export default LollyTextUndefinedComponent
